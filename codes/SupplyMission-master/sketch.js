3ac3e5908be97e792d45c30a025f30673b7d7123
KARNAMROOPESH <karnamroop1234@gmail.com>
2020-04-28 16:16:27 +0530

    simple changes made

--- codes/SupplyMission-master/sketch.js
+++ codes/SupplyMission-master/sketch.js
@@ -47,8 +47,7 @@ function setup() {
 }
 
 
-function draw() {
-  rectMode(CENTER);
+function draw() { rectMode(CENTER);
   background(0);
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y
